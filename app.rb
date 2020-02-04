c20a81c29197e0b522ee427f1b4da2a95b931d8a
asengelhart <asengelhart@yahoo.com>
2020-02-04 16:49:51 -0700

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,22 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    get '/' do
+      erb :root
+    end
 
-    # code other routes/actions here
+    get '/new' do
+      erb :"pirates/new"
+    end
 
+    post '/pirates' do
+      pirate_hash = params["pirate"]
+      ship_hash1 = params["pirate"]["ships"][0]
+      ship_hash2 = params["pirate"]["ships"][1]
+      @pirate = Pirate.new(pirate_hash["name"], pirate_hash["weight"], pirate_hash["height"])
+      @ship1 = Ship.new(ship_hash1["name"], ship_hash1["type"], ship_hash1["booty"])
+      @ship2 = Ship.new(ship_hash2["name"], ship_hash2["type"], ship_hash2["booty"])
+      erb :"pirates/show"
+    end
   end
 end
